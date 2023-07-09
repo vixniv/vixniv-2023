09030935c93965daee989d9f3c30c37556e109ae
vixniv <vixniv@gmail.com>
2023-07-10 00:40:44 +0700

    fix delay in bot response

--- src/utils/botResponse.ts
+++ src/utils/botResponse.ts
@@ -3,14 +3,14 @@ import { delay } from "./delay";
 import { IChatGroup } from "./interfaces";
 import { pushMessage } from "./pushMessage";
 
-export const botResponse = async (
+export const botResponse = (
   setChat: React.Dispatch<React.SetStateAction<IChatGroup[]>>,
   command: string
 ) => {
   const commandIndex = availableCommands.indexOf(command);
   if (commandIndex !== -1) {
-    await delay(500);
-    data[commandIndex]?.response.message?.forEach((message) => {
+    data[commandIndex]?.response.message?.forEach(async (message, index) => {
+      await delay(500 * (index + 1));
       pushMessage(setChat, message, "bot");
     });
   }
