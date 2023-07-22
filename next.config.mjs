092f659c62dd457923e3a477f4b02c52d5b5e4cb
vixniv <vixniv@gmail.com>
2023-07-22 23:19:44 +0700

    fix conflict next.config typescript warn

--- next.config.mjs
+++ next.config.mjs
@@ -20,17 +20,17 @@ const config = {
   },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
+    // const fileLoaderRule = config.module.rules.find((rule) =>
+    //   rule.test?.test?.(".svg")
+    // );
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
+      // {
+      //   ...fileLoaderRule,
+      //   test: /\.svg$/i,
+      //   resourceQuery: /url/, // *.svg?url
+      // },
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
@@ -41,7 +41,7 @@ const config = {
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+    // fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
